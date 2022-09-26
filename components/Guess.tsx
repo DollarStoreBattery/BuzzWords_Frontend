d0ed0f701e9583824704b92787ee829bedd58a1a
Wajiha Shah <41492355+DollarStoreBattery@users.noreply.github.com>
2022-09-26 17:37:21 -0400

    animate guess text on change

--- components/Guess.tsx
+++ components/Guess.tsx
@@ -1,21 +1,31 @@
 /** @jsxImportSource @emotion/react */
 import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
 import usePlaySessionStore from "../lib/usePlaySessionStore";
 import TextElement from "./basic/TextElement";
+
+const pulse = keyframes`
+0% {transform: translateX(3%);}
+100% {transform:translateX(0%) }
+`;
 
 const GuessText = styled(TextElement)({
   fontSize: "4rem",
   // todo: add media queries for font size
-  //   todo: get the text to animate when cleared, erased, appended, etc. below is not working
   transition: ".1s linear",
   height: "auto",
   minHeight: "50px",
   maxWidth: "100%",
+  animation: `${pulse} 100ms ease`,
 });
 
 const Guess = () => {
   const currentGuess = usePlaySessionStore((state) => state.currentGuess);
-  return <GuessText size="xl">{currentGuess.toUpperCase()}</GuessText>;
+  return (
+    <GuessText size="xl" key={currentGuess}>
+      {currentGuess.toUpperCase()}
+    </GuessText>
+  );
 };
 
 export default Guess;
